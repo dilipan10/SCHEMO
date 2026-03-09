bd053e4020d958575a3452b9c330d08fa5f36720
Haripriya-Hp-38 <haripriyasakthivel038@gmail.com>
2026-03-09 14:48:13 +0530

    Delete backend/routes/userRoutes.js

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signupUser, loginUser } = require('../controllers/userController');
-
-// Define user routes
-router.post('/signup', signupUser);
-router.post('/login', loginUser);
-
-module.exports = router;
